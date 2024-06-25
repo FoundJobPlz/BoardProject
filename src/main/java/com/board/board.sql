b3337d398a53941b2572ba5346209e1ec492ba18
Hwan <ic0226@naver.com>
2024-06-26 01:13:49 +0900

    refactor : ListBoardResponse 수정, board.sql 삭제
    일급컬렉션 활용한 ListBoardResponse 수정
    초기화, 권한 설정이 따로 필요없어 board.sql 삭제

--- src/main/java/com/board/board.sql
+++
@@ -1,3 +0,0 @@
-create DATABASE mydb;
-GRANT ALL PRIVILEGES ON mydb.* TO 'root'@'localhost';
-FLUSH PRIVILEGES;
--- src/main/java/com/board/controller/ListBoardResponse.java
+++ src/main/java/com/board/controller/ListBoardResponse.java
@@ -1,19 +1,12 @@
 package com.board.controller;
 
 import com.fasterxml.jackson.annotation.JsonValue;
+import lombok.RequiredArgsConstructor;
 
 import java.util.List;
 
+@RequiredArgsConstructor
 public class ListBoardResponse {
 
-    private final List<ListBoardResponseDto> boardList;
-
-    public ListBoardResponse(List<ListBoardResponseDto> boardList) {
-        this.boardList = boardList;
-    }
-
-    @JsonValue
-    public List<ListBoardResponseDto> getBoardList() {
-        return boardList;
-    }
+    private final List<ListBoardResponseDto> list;
 }
